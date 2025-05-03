36437337d42538096b19067fdcb0a6b4a6c32cfe
Espereon <announcement32@post.com>
2025-05-04 00:49:11 +0300

    test pinia

---
+++ src/App.vue
@@ -0,0 +1,39 @@
+<script setup>
+import { useCounterStore, useFetcherStore } from "./store/index";
+import { onMounted } from "vue";
+
+const counter = useCounterStore();
+const fetcher = useFetcherStore();
+
+onMounted(() => {
+  fetcher.fetchItems();
+});
+</script>
+
+<template>
+  <div>
+    <h1>Count is {{ counter.count }}</h1>
+    <button @click="counter.increment(5)">Увеличение</button>
+    <button @click="counter.waitAndAdd">Увеличение с задержкой</button>
+    <div v-for="item in fetcher.items" :key="item.id">
+      <p>Наименование товара {{ item.name }}</p>
+      <b> Цена {{ item.price }} </b>
+      <span> Год производства {{ item.year }}</span>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
---
+++ src/store/index.js
@@ -0,0 +1,41 @@
+import { defineStore } from "pinia";
+import axios from "axios";
+
+export const useCounterStore = defineStore("counter", {
+  state: () => {
+    return {
+      count: 0,
+    };
+  },
+  actions: {
+    increment(i) {
+      this.count += i;
+    },
+    async waitAndAdd() {
+      setTimeout(() => {
+        this.count++;
+      }, 2000);
+    },
+  },
+  getters: {
+    doubleCount: (state) => state.count * 2,
+  },
+});
+
+export const useFetcherStore = defineStore("data", {
+  state: () => ({
+    items: [],
+  }),
+  actions: {
+    async fetchItems() {
+      try {
+        const response = await axios.get(
+          "https://13c7dcadda889182.mokky.dev/items"
+        );
+        this.items = response.data;
+      } catch (err) {
+        console.error(err);
+      }
+    },
+  },
+});
